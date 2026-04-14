da3e9c1dbdb41bb8d4991bc5443ad7a516b2a094
Jonas Pohl <jonas.jp4c@gmail.com>
2026-04-14 14:46:12 +0200

    Fixing Serialization problems in nn (#29)
    Co-authored-by: Tilman Holube <tilman@holube.de>

--- zis-ai/src/test/java/org/schlunzis/zis/ai/nn/NeuralNetworkTest.java
+++ zis-ai/src/test/java/org/schlunzis/zis/ai/nn/NeuralNetworkTest.java
@@ -3,6 +3,9 @@ package org.schlunzis.zis.ai.nn;
 import org.junit.jupiter.api.Test;
 import org.schlunzis.zis.math.linear.Matrix;
 
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class NeuralNetworkTest {
@@ -37,5 +40,34 @@ class NeuralNetworkTest {
                     " | Expected: " + expected + " | Predicted: " + predicted);
         }
     }
+
+    @Test
+    void testLoad() throws IOException, ClassNotFoundException {
+        NeuralNetwork nn = NeuralNetwork.deserialize(new File("src/test/resources/xor.nn"));
+        for (Matrix[] xOrDatum : xOrData) {
+            Matrix input = xOrDatum[0];
+            double predicted = nn.query(input).get(0, 0);
+            double expected = xOrDatum[1].get(0, 0);
+            assertTrue(Math.abs(expected - predicted) < 0.2, "Input: " + input.get(0, 0) + ", " + input.get(0, 1) +
+                    " | Expected: " + expected + " | Predicted: " + predicted);
+        }
+    }
+
+    @Test
+    void testSave() throws IOException, ClassNotFoundException {
+        NeuralNetwork nn = new NeuralNetwork(42, 0.01, 2, 1, 2);
+        nn.setActivationFunction(NeuralNetwork.tanh);
+        // Train the neural network
+        int epochs = 5000;
+        for (int epoch = 0; epoch < epochs; epoch++) {
+            for (Matrix[] xOrDatum : xOrData) {
+                nn.train(xOrDatum[0], xOrDatum[1]);
+            }
+        }
+        // Save the neural network
+        File file = new File("/tmp/xor_saved.nn");
+        nn.serialize(file);
+    }
+
 
 }
